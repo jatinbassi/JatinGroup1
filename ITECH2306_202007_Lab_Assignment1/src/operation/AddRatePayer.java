6f39c6c05ed23907aee6e29543d425c405f6eacf
amritpalramana <62825768+amritpalramana@users.noreply.github.com>
2020-05-25 20:53:10 +1000

    amrit changed to addRatePayer

--- ITECH2306_202007_Lab_Assignment1/src/operation/AddRatePayer.java
+++ ITECH2306_202007_Lab_Assignment1/src/operation/AddRatePayer.java
@@ -1,29 +1,59 @@
 package operation;
+import java.io.File;
 
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import domain.RatePayer;
+
 /**
- * @author Takeogh
- 
+ * @author Amritpal kaur ramana
  * @version 1.0
- * @created 02-Apr-2020 8:30:00am
+ * @created 20 may, 2020
  */
 public class AddRatePayer extends FunctionalDialog {
-
+	
+	RatePayerData rateData ;
 	public AddRatePayer(Scanner console) {
 		super(1, console);
+		rateData = new RatePayerData();
+		rateData.readFromFile();
 	}
 
 	@Override
 	public void obtainInput(int i) {
-		System.out.println("Not implemented yet");
+
+		String name, address, postcode, phone, type;
+		System.out.println("Enter Rate Payer's name : ");
+		name = super.getScanner().nextLine();
+		System.out.println("Enter Rate Payer's Address : ");
+		address = super.getScanner().nextLine();
+		System.out.println("Enter Rate Payer's postcode : ");
+		postcode = super.getScanner().nextLine();
+		System.out.println("Enter Rate Payer's phone : ");
+		phone = super.getScanner().nextLine();
+		System.out.println("Enter Rate Payer's property type : ");
+		type = super.getScanner().nextLine();
+		
+		RatePayer ratePayer = new RatePayer();
+		ratePayer.setName(name);
+		ratePayer.setAddress(address);
+		ratePayer.setPhone(phone);
+		ratePayer.setPostcode(postcode);
+		ratePayer.setType(type);
+		rateData.insert(ratePayer);
 		setStillRunning(false);
 	}
 
 	@Override
 	public void respondToInput() {
-		// TODO Auto-generated method stub
+		
+		System.out.println("\nList of RatePayers is : \n"+rateData.toString());
+		rateData.writeToFile();
 		
 	}
-
+	
+	
 }
