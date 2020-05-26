4a15efa2538b93c776a9bd199a0af40f2b8b91e8
jatinbassi <63133006+jatinbassi@users.noreply.github.com>
2020-05-26 22:42:07 +1000

    edit by jatin
    edit by jatin

--- ITECH2306_202007_Lab_Assignment1/src/operation/AddProperty.java
+++ ITECH2306_202007_Lab_Assignment1/src/operation/AddProperty.java
@@ -1,28 +1,71 @@
-package operation;
+ackage operation;
+import java.io.File;
 
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import domain.OtherProperty;
+import domain.Property;
+import domain.Residential;
+import domain.AddProperty;
+
 /**
- * @author Takeogh
- * @version 1.0
- * @created 02-Apr-2020 8:30:00am
+ * @author jatin bassi
+ * @version 1.1
+ * @created 21 may, 2020
  */
 public class AddProperty extends FunctionalDialog {
-
+	
+	CalculatePropertyTypeRates propertydata;
 	public AddProperty(Scanner console) {
-		super(1,console);
+		super(1, console);
+		propertydata = new CalculatePropertyTypeRates();
+		propertydata.readFromFile();
 	}
 
 	@Override
 	public void obtainInput(int i) {
-		System.out.println("Not implemented yet");
-		setStillRunning(false);	
+
+String unit,address,area,siteValue,capitalImprovedValue,capitalImprovedRate,netAnnualValue,valuationDate;
+		System.out.println("Enter units's name : ");
+		name = super.getScanner().nextLine();
+		System.out.println("Enter property's Address : ");
+		address = super.getScanner().nextLine();
+		System.out.println("Enter property area : ");
+		postcode = super.getScanner().nextLine();
+		System.out.println("Enter property's siteValue : ");
+		phone = super.getScanner().nextLine();
+		System.out.println("Enter property's capitalImprovedValue : ");
+		type = super.getScanner().nextLine();
+		System.out.println("Enter property's capitalImprovedRate : ");
+		type = super.getScanner().nextLine();
+		System.out.println("Enter property's netAnnualValue : ");
+		type = super.getScanner().nextLine();
+		System.out.println("Enter property's valuationDate : ");
+		type = super.getScanner().nextLine();
+		
+		AddProperty addproperty = new AddProperty();
+		addproperty.setUnit(unit);
+		addproperty.setAddress(address);
+		addproperty.setArea(area);
+		addproperty.setSiteValue(siteValue);
+		addproperty.setCapitalImprovedValue(capitalImprovedValue);
+		addproperty.setCapitalImprovedRate(capitalImprovedRate);
+		addproperty.setNetAnnualValue(netAnnualValue);
+		addproperty.setValuationDate(valuationDate);
+		propertydata.insert(ratePayer);
+		setStillRunning(false);
 	}
 
 	@Override
 	public void respondToInput() {
-		// TODO Auto-generated method stub
+		
+		System.out.println("\nList of Property type is : \n"+propertydata.toString());
+		propertydata.writeToFile();
 		
 	}
-
+	
+	
 }
